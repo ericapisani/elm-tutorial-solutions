202deef735b2d94619f609492d225ffd5bc3505a
Erica Pisani <pisani.erica@gmail.com>
2017-07-14 15:57:01 -0400

    Add the rest of the code for randomly assigned a new 'dice face' value to the model.

--- Effects/Random.elm
+++ Effects/Random.elm
@@ -1,6 +1,14 @@
 import Html exposing (..)
-import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Random
+
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
 
 
 -- MODEL
@@ -17,11 +25,12 @@ type Msg
 -- UPDATE
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
+  -- Produce a random number from 1 to 6, and fire a message of type NewFace with the value
   case msg of
     Roll ->
       (model, Random.generate NewFace (Random.int 1 6))
 
-    NewFace ->
+    NewFace newFace ->
       (Model newFace, Cmd.none)
 
 
@@ -32,6 +41,12 @@ view model =
     [ h1 [] [ text (toString model.dieFace) ]
     , button [ onClick Roll ] [ text "Roll" ]
     ]
+
+
+-- SUBSCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
 
 
 -- INIT
